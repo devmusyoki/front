c567b708e17b0a8ea8ca1e109fcbab4b52c3c81a
smusyoki <markmusyoki6@gmail.com>
2021-06-30 16:46:02 +0300

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
+  mode: "jit",
   purge: [
-		".pages/**/*.{js,ts,jsx,tsx}",
-		".components/**/*.{js,ts,jsx,tsx}",
-	],
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   variants: {
     extend: {},
   },
+  // eslint-disable-next-line global-require
   plugins: [],
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
 }
