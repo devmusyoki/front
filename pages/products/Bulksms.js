211989d8a21e4ae003ed8dfe37a3c37a46248059
smusyoki <markmusyoki6@gmail.com>
2021-07-11 17:43:57 +0300

    changes

--- pages/products/Bulksms.js
+++ pages/products/Bulksms.js
@@ -7,7 +7,7 @@ const Bulk_sms = () => {
          < Navbar />
       <div className="relative top-16 bg-blue-300 overflow-hidden">
       <div className="max-w-7xl mx-auto">
-      <div className="relative z-10 pb-8 bg-blue-300 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+      <div className="relative z-10 pb-8 bg-blue-300 sm:pb-16 md:pb-20 w-full lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
       <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-blue-300 transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
         <polygon points="50,0 100,0 50,100 0,100" />
       </svg>
